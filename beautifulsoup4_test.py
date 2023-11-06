7f3bea0eaa1a47841b9842c9948597c46ea60bf0
Valeriy Lvov <tepliy_shov@mail.ru>
2023-11-06 10:43:20 +0400

    get image

--- beautifulsoup4_test.py
+++ beautifulsoup4_test.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = 'https://tululu.org/b1/'
+url = 'https://tululu.org/b9/'
 response = requests.get(url)
 response.raise_for_status()
 soup = BeautifulSoup(response.text, 'lxml')
-title_tag = soup.find('title')
-print(title_tag.text)
+image_element = soup.find('img', src='/shots/9.jpg')['src']
+print(f'https://tululu.org{image_element}')
